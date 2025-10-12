7b0dd32c8ccb4625a5b1ec76c95b952eab2ca923
arvinr16 <arviansyahyk@gmail.com>
2025-10-12 12:37:33 +0700

    Adding variable for Typewriter effect in 'main.js'

--- src/main.js
+++ src/main.js
@@ -13,3 +13,17 @@ bg.addEventListener('mouseleave', () => {
     bg.style.cursor = 'grab';
 });
 // Grabbing effect in Header End
+
+// Typewriter effect in Landing Start
+const texts = ["Selamat datang diwebsite Gerakan Subsidi Buku",
+                "Gerakan yang bertujuan untuk memberikan harga buku yang terjangkau",
+                "Join with us"
+];
+
+let x = 0;
+let y = 0;
+let currentText = texts[y];
+let isDeleting = false;
+const speed = 80;
+const deleteSpeed = 50;
+const delay = 1000;
